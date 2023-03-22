9e2d7b75d2da2e9e78e1f348ef40877a34724195
MadhugandhaChavan45 <madhu.chavan98@gmail.com>
2023-03-22 18:47:55 +0530

    Commit for checking ngrok5

--- src/test/java/calculator/test.java
+++ src/test/java/calculator/test.java
@@ -7,13 +7,13 @@ public class test {
     public void test_squareRoot() {
         double a = 484.0;
         double b = 1225.0;
-        double c = 169.0;
+//        double c = 169.0;
         double result1 = Main.squareRoot(a);
         double result2 = Main.squareRoot(b);
-        double result3 = Main.squareRoot(c);
+//        double result3 = Main.squareRoot(c);
         Assert.assertEquals(22, result1, 0.0f);
         Assert.assertEquals(35, result2, 0.0f);
-        Assert.assertEquals(13, result3, 0.0f);
+//        Assert.assertEquals(13, result3, 0.0f);
     }
 
     @Test
